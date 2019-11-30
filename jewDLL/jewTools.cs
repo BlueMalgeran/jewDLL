ae89a8d9079a28cd56345c091573ef7282dd6c55
Blue Malgeran <yos7816@gmail.com>
2019-11-30 20:04:44 +0200

    [v1.0] jewTools Class - Contains, Digits
    Added:
    + jewTools Class
       + Contains function
       + Digits function
    Changed:
    • None.
    Removed:
    - jewMath class and everything in it

---
+++ jewDLL/jewTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace jewDLL
+{
+    public class jewTools
+    {
+        public static bool Contains<T>(T content, T term)
+        {
+            bool result = content.ToString().Contains(term.ToString());
+            return result;
+        }
+
+        public static int Digits<T>(T args)
+        {
+            int result = args.ToString().Length;
+            return result;
+        }
+    }
+}
